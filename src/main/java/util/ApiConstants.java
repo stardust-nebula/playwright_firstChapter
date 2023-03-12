87a4f3b4fd2d91a8dc1debda07229b44f80e4912
lera <leramulugeta@gmail.com>
2023-03-12 21:26:48 +0300

    added missing ApiConstants class

---
+++ src/main/java/util/ApiConstants.java
@@ -0,0 +1,13 @@
+package util;
+
+public class ApiConstants {
+
+    private ApiConstants(){}
+
+    public static final String BASE_API_URL = "https://restful-booker.herokuapp.com";
+    public static final String BOOKING_ENDPOINT = "/booking";
+    public static final String BOOKING_ID_ENDPOINT = "/%s";
+    public static final String AUTH_ENDPOINT = "/auth";
+
+    public static final String BASE_URL_INTERNET_HEROKUAPP = "http://the-internet.herokuapp.com/login";
+}
